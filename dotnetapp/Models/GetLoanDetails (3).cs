0c7f842f4985dd4f61407429aad482094e42b367
PSLSarvani <sailakshmisarvanipolisetty@gmail.com>
2023-07-24 08:21:40 +0000

    models

---
+++ dotnetapp/Models/GetLoanDetails (3).cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetapp.Models
+{
+    public class GetLoanDetails
+    {
+        public string loantype { get; set; }
+        public string applicantName { get; set; }
+        public string applicantAddress { get; set; }
+        public string applicantMobile { get; set; }
+        public string applicantEmail { get; set; }
+        public string applicantAadhaar { get; set; }
+        public string applicantPan { get; set; }
+        public string applicantSalary { get; set; }
+        public string loanAmountRequired { get; set; }
+        public string loanRepaymentMonths { get; set; }
+    }
+    public class LoanRequestModel
+    {
+        public int LoanId { get; set; }
+        public GetLoanDetails LoanDetails { get; set; }
+    }
+}
