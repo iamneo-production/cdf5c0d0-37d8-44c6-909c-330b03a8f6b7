a2d2d6b9d5475fe43caebcd31455c1d742f1795e
Sukesh510 <sukeshsundar510@gmail.com>
2023-07-24 10:06:17 +0000

    Dashboard Correction on #14

--- reactapp/src/Dashboard.jsx
+++ reactapp/src/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import { Route, Link, Routes } from 'react-router-dom';
 import Customerprofile from './user/Customerprofile';
 import Appliedstatus from "./user/Appliedstatus";
@@ -7,26 +7,28 @@ import { GetDocument } from "./user/GetDocument";
 import { LoanApplied } from "./user/LoanApplied";
 
 const Dashboard = () => {
-  const handleLogout = {}
+  const handleLogout=() => {
+    console.log("Log out");
+  }
   return (
     <>
-     <header>
-        <h1>Bike Loan</h1> 
-      <nav>
-        <ul>
-        <li>
-          <Link to="/user/applyLoan">Apply Loan</Link>
-        </li>
-        <li>
-            <Link to="/user/viewLoan">Loan Status</Link>
-          </li>
-          <li>
-            <Link to="/user/getProfile">Profile</Link>
-          </li>
-        </ul>
-      </nav>
-      <button className="logout-button" onClick={handleLogout}>Logout</button>
-      </header> 
+      <header>
+        <h1>Bike Loan</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/user/applyLoan">Apply Loan</Link>
+            </li>
+            <li>
+              <Link to="/user/viewLoan">Loan Status</Link>
+            </li>
+            <li>
+              <Link to="/user/getProfile">Profile</Link>
+            </li>
+          </ul>
+        </nav>
+        <button className="logout-button" onClick={handleLogout}>Logout</button>
+      </header>
       <Routes>
         <Route exact path="/user/getProfile" element={<Customerprofile />} />
         <Route exact path="/user/viewLoan" element={<Appliedstatus />} />
