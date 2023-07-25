b6d081fd24f2c674a463bb376ad8c3a7c083b167
PSLSarvani <sailakshmisarvanipolisetty@gmail.com>
2023-07-25 11:42:53 +0000

    LoginAuth Correction

--- reactapp/src/Auth/LoginAuth.jsx
+++ reactapp/src/Auth/LoginAuth.jsx
@@ -1,6 +1,6 @@
 function LoginAuth(values){
     let errorMsg = {}
-    const password_pattern = /^[a-zA-Z0-9]{2,}$/
+    // const password_pattern = /^[a-zA-Z0-9]{2,}$/
     //const password_pattern = /^(?=.\d)(?=.[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/
 
     if(values.email === "") {
