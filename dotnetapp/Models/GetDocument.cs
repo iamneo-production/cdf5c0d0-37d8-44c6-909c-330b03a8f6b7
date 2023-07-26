9a72ae9036573bfda441b2fdb4d8a29444d82e1c
S-Chandru <113829052+S-Chandru@users.noreply.github.com>
2023-07-26 15:25:26 +0530

    Add files via upload

---
+++ dotnetapp/Models/GetDocument.cs
@@ -0,0 +1,9 @@
+﻿namespace BikeLoan.Model
+{
+    public class GetDocument
+    {
+        public int DocumentId { get; set; }
+        public string DocumentType { get; set; }
+        public IFormFile DocumentUpload { get; set; }
+    }
+}
