f5fe2ef2b8c619d6a1f1a35340103a59fe72b1df
PSLSarvani <131974860+PSLSarvani@users.noreply.github.com>
2023-07-26 14:54:36 +0530

    Add files via upload

---
+++ dotnetapp/Models/GetLoanDetails.cs
@@ -0,0 +1,16 @@
+﻿namespace BikeLoan.Model
+{
+    public class GetLoanDetails
+    {
+        public string loantype { get; set; }
+        public string applicantName { get; set; }
+        public string applicantAddress { get; set; }
+        public string applicantMobile { get; set; }
+        public string applicantEmail { get; set; }
+        public string applicantAadhaar { get; set; }
+        public string applicantPan { get; set; }
+        public double applicantSalary { get; set; }
+        public double loanAmountRequired { get; set; }
+        public int loanRepaymentMonths { get; set; }
+    }
+}
