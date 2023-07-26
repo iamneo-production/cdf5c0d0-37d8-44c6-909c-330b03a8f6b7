cff23d31bed2734b1ed6e481ec9714ef8c3ee1ee
PSLSarvani <131974860+PSLSarvani@users.noreply.github.com>
2023-07-26 14:55:36 +0530

    Add files via upload

---
+++ dotnetapp/Models/Document.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BikeLoan.Model
+{
+    public class Document
+    {
+        [Required, Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int DocumentId { get; set; }
+        public string DocumentType { get; set; }
+        public string DocumentName { get; set; }
+        public byte[] DocumentUpload { get; set; }
+    }
+}
